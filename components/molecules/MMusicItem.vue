f8191712b53ce580da06147a923b001d11ba924d
Akihiro <marsstay0729@gmail.com>
2021-03-04 12:13:54 +0900

    Add remove button in  MusicItem component

--- components/molecules/MMusicItem.vue
+++ components/molecules/MMusicItem.vue
@@ -1,5 +1,9 @@
 <template>
-  <div :class="$style.wrapper">
+  <div
+    :class="$style.wrapper"
+    @mouseenter="changeHoverFlag(true)"
+    @mouseleave="changeHoverFlag(false)"
+  >
   <picture>
     <img src="@/assets/images/sample.jpg" alt="" :class="$style.img" />
   </picture>
@@ -10,25 +14,37 @@
       <div :class="$style.time">時間：4:32</div>
     </div>
 
-    <div class="iconTimerStart" :class="$style.startIcon" />
+    <div :class="$style.iconBox">
+      <div class="iconTimerStart" :class="$style.startIcon" />
+    </div>
 
     <!-- hover時表示 -->
-    <!-- <div>×</div> -->
+    <div v-if="isHover" :class="$style.remove">×</div>
   </div>
 </template>
 
-<script>
-import { defineComponent } from '@nuxtjs/composition-api'
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
-    return {}
+    const isHover = ref(true)
+
+    const changeHoverFlag = (flag: boolean) => {
+      isHover.value = flag
+    }
+
+    return {
+      isHover,
+      changeHoverFlag
+    }
   },
 })
 </script>
 
 <style lang="scss" module>
 .wrapper {
+  position: relative;
   display: grid;
   grid-template-columns: 4.1rem 68% auto;
   grid-column-gap: 1rem;
@@ -53,9 +69,22 @@ export default defineComponent({
   font-size: 0.75rem;
 }
 
-.startIcon {
-  font-size: 1.625rem;
+.iconBox {
   display: grid;
   place-items: center;
+}
+
+.startIcon {
+  font-size: 1.625rem;
+}
+
+.remove {
+  position: absolute;
+  top: 0;
+  right: 0.25rem;
+
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
